dfc8d4d83ab6887f3b1b54dc96aed2e925aaec77
DinhhHau <dinhhaunguyen0812@gmail.com>
2022-10-12 23:33:39 +0700

    layout_component

--- src/pages/UserDetail/UserDetail.tsx
+++ src/pages/UserDetail/UserDetail.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type Props = {};
 
 export default function UserDetail({}: Props) {
-  return <div className="mt-5">UserDetail</div>;
+  return <div className="my-5">UserDetail</div>;
 }
